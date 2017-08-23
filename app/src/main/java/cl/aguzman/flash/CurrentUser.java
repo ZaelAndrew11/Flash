f63aa0cc8a24efb6ee4cc0fa906b3b0864f0995f
Alfonso <legion1193@gmail.com>
2017-08-23 01:02:29 -0300

    Flash App

---
+++ app/src/main/java/cl/aguzman/flash/CurrentUser.java
@@ -0,0 +1,12 @@
+package cl.aguzman.flash;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CurrentUser {
+    private FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    public FirebaseUser getCurrentUser() {
+        return currentUser;
+    }
+}
